78a40919da2c05f86047ce6cb64ced3ff49c4658
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-03 01:56:07 +0200

    Fixed BrowseDrop

--- src/components/headerComponents/BrowseDrop.js
+++ src/components/headerComponents/BrowseDrop.js
@@ -2,29 +2,43 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function BrowseDrop() {
-  const [menLinks, setMenLinks] = useState([]);
-  const [womenLinks, setWomenLinks] = useState([]);
+  const [menLinks] = useState([
+    'Dresses',
+    'Tops',
+    'Sweaters/Knits',
+    'Jackets/Coats',
+    'Blazers',
+    'Denim',
+    'Leggings/Pants',
+    'Skirts/Shorts',
+    'Accessories'
+  ]);
+  const [womenLinks] = useState([
+    'Tees/Tank tops',
+    'Shirts/Polos',
+    'Sweaters',
+    'Sweatshirts/Hoodies',
+    'Blazers',
+    'Jackets/vests'
+  ]);
 
   //TODO: Fetching links/filling routes path
-
-  setMenLinks([]);
-  setWomenLinks([]);
 
   return (
     <div className="drop__browse">
       <div className="drop__browse__flex">
         <h3 className="drop__h3">Women</h3>
         <ul className="drop__menu">
-          {womenLinks.map(link => <li key={link.id}>
-            <Link to={link.route} tag="a" >link.name</Link>
+          {womenLinks.map((link, idx) => <li key={idx}>
+            <Link className="drop__link link" to="/products" tag="a" >{link}</Link>
           </li>)}
         </ul>
       </div>
       <div className="drop__flex">
         <h3 className="drop__h3">Men</h3>
         <ul className="drop__menu">
-          {menLinks.map(link => <li key={link.id}>
-            <Link to={link.route} tag="a" >link.name</Link>
+          {menLinks.map((link, idx) => <li key={idx}>
+            <Link className="drop__link link" to="/products" tag="a" >{link}</Link>
           </li>)}
         </ul>
       </div>
