2c9badc84e17b4ff6dcd8b7415ebe26ee90302e3
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-02 18:51:50 +0200

    HeaderCartItem.js component

---
+++ src/components/headerComponents/HeaderCartItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+//TODO: store integration
+
+function HeaderCartItem({ item }) {
+  return (
+    <div className="cart__drop_item">
+      <div className="cart__drop__description">
+        <img src={item.img}  alt={item.name}/>
+        <div className="cart__drop__description_text">
+          <h4><Link to={`/products/${item.id}`} className="cart__link link">{item.name}</Link></h4>
+          <img src={item.rating} alt="item rating"
+            className="cart__star" />
+          <p className="cart__text">1 x $250</p>
+        </div>
+      </div>
+      <button className="cart__delete"><i
+        className="fas fa-times-circle" /></button>
+    </div>
+  );
+}
+
+HeaderCartItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    img: PropTypes.string,
+    rating: PropTypes.string
+  })
+};
