836d3966ac1fa3c112ac6f17594fce0cf559dab3
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-02 04:56:17 +0200

    Root reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,11 @@
-import {compose} from "redux";
+import { combineReducers } from 'redux';
+import { itemsReducer } from './itemsReducer';
+import { authReducer } from './authReducer';
+import { cartReducer } from './cartReducer';
 
-export const rootReducer = () => {
-    compose()
-}
+export const rootReducer = combineReducers({
+  items: itemsReducer,
+  cart: cartReducer,
+  user: authReducer
+},
+);
