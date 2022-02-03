c3e5fb50af168262f188d828f40da42a4aeed8a0
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-03 02:00:39 +0200

    Fix imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Search from './headerComponents/Search';
 import { Link } from 'react-router-dom';
 import HeaderCart from './headerComponents/HeaderCart';
+import logo from '../assets/img/logo.png';
+import cart from '../assets/img/cart.svg';
 
 
 export default function Header() {
@@ -10,14 +12,14 @@ export default function Header() {
       <div className="container header__content">
         <div className="header__left">
           <Link className="logo" to="/" tag="a">
-            <img alt="logo" className='logo__img' src="../assets/img/logo.png" />
+            <img alt="logo" className='logo__img' src={logo}/>
             <p>BRAN<span>D</span></p>
           </Link>
           <Search />
         </div>
         <div className="header__right">
           <Link to="/cart" tag="a">
-            <img alt="cart" className='cart' src="../assets/img/cart.svg" />
+            <img alt="cart" className='cart' src={cart} />
           </Link>
           <HeaderCart />
           <Link className="my_acc" to="/cart">My account <i className="fas fa-caret-down" /></Link>
