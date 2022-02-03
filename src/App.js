d20a4c279a06903be5b01424adab338d84baa7fb
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-03 02:17:04 +0200

    HomePage view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 //import { useRoutes } from './router';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import HomePage from './views/HomePage';
 
 function App() {
   //const isAuthenticated = false;
@@ -13,6 +14,7 @@ function App() {
       <div className="app">
         <Header />
         <Navbar />
+        <HomePage />
       </div>
     </Router>
   );
---
+++ src/views/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeBanner } from '../components/HomeBanner';
+import { OfferBanner } from '../components/OfferBanner';
+
+export default function HomePage() {
+  return <>
+    <HomeBanner />
+    <OfferBanner />
+  </>;
+}
