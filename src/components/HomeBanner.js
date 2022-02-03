132d7dfc161e521b2f46c94777035f771a5f7ad0
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-03 02:17:23 +0200

    Static banners for home

---
+++ src/components/HomeBanner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomeBanner = () => <section className="promo">
+  <div className="container promo__content">
+    <div className="promo__text">
+      <h2 className='promo__text_big'>THE BRAND</h2>
+      <h2>OF LUXERIOUS <span style={{ color: '#f16d7f' }}>FASHION</span></h2>
+    </div>
+  </div>
+</section>;
---
+++ src/components/OfferBanner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import man from '../assets/img/man.png';
+import woman from '../assets/img/woman.png';
+import kid from '../assets/img/kid.png';
+import acc from '../assets/img/acc.png';
+
+export const OfferBanner = () => <div className="offer container">
+  <div className="offer__content">
+    <div className="offer__content_big" style={{ backgroundImage: `url(${man})` }}>
+      <div className="offer__text">
+        <p>hot deal</p>
+        <p><span>for men</span></p>
+      </div>
+    </div>
+    <div className="offer__content_mini" style={{ backgroundImage: `url(${acc})` }}>
+      <div className="offer__text">
+        <p>LUXIROUS & trendy</p>
+        <p><span>ACCESORIES</span></p>
+      </div>
+    </div>
+  </div>
+  <div className="offer__content">
+    <div className="offer__content_mini"
+      style={{ backgroundImage: `url(${woman})` }}>
+      <div className="offer__text">
+        <p>30% offer</p>
+        <p><span>women</span></p>
+      </div>
+    </div>
+    <div className="offer__content_big"
+      style={{ backgroundImage: `url(${kid})` }}>
+      <div className="offer__text">
+        <p>new arrivals</p>
+        <p><span>FOR kids</span></p>
+      </div>
+    </div>
+  </div>
+</div>;
