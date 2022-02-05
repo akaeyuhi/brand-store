964a0107f6466a2ff75ac3344642058d5fc28010
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 21:10:15 +0200

    Delete comments

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style.sass';
 import App from './App';
-//import { store } from './store';
-//import { Provider } from 'react-redux';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/*<Provider store={store}>
-    </Provider>*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
