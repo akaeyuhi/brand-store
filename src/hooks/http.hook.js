2bf844b725a6f00214a4e097e0d887e0a8b51e36
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 21:36:08 +0200

    useHttp hook to do auth requests

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const useHttp = token => {
+  const [auth, setAuth] = useState(token);
+
+  async function authFetch(url, method = 'GET', data = {}) {
+    try {
+      return await (await fetch(url, {
+        method,
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: auth
+        }
+      })).json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return {
+    setAuth, authFetch
+  };
+};
