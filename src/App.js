b748dc055e166ca1f6748487c083a1649fc6bb7b
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 21:14:39 +0200

    Add router to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-//import { useRoutes } from './router';
+import { useRoutes } from './router';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import HomePage from './views/HomePage';
+import About from './components/About';
+import { Footer } from './components/Footer';
 
 function App() {
-  //const isAuthenticated = false;
-  //const routes = useRoutes(isAuthenticated);
+  const isAuthenticated = false;
+  const routes = useRoutes(isAuthenticated);
 
   return (
     <Router>
       <div className="app">
         <Header />
         <Navbar />
-        <HomePage />
+        {routes}
+        <About />
+        <Footer />
       </div>
     </Router>
   );
