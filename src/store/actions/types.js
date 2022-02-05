aecb47090d6281cf26f6d6725c2a47d4363e04d5
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 21:36:40 +0200

    Delete unnecessary type

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -7,4 +7,3 @@ export const CART_ADD = 'CART_ADD';
 export const CART_DELETE = 'CART_DELETE';
 export const CART_UPDATE = 'CART_UPDATE';
 export const CART_CLEAR = 'CART_CLEAR';
-export const EMAIL_SUBSCRIBE = 'EMAIL_SUBSCRIBE';
