38af990d3250461981bc74073aeab03049f81f27
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 17:48:37 +0200

    Add page import

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes  } from 'react-router-dom';
+import HomePage from '../views/HomePage';
 /* eslint-disable react/jsx-no-undef */
 
 export const useRoutes = isAuthenticated => {
