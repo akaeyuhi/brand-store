b7aa74a76e2757435c72847e1289c6c95371c007
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-05 18:15:09 +0200

    Update Home page

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { HomeBanner } from '../components/HomeBanner';
 import { OfferBanner } from '../components/OfferBanner';
+import FeaturedItems from '../components/FeaturedItems';
+import FeaturesBanner from '../components/FeaturesBanner';
+import Subscribe from '../components/Subscribe';
 
 export default function HomePage() {
   return <>
     <HomeBanner />
     <OfferBanner />
+    <FeaturedItems />
+    <FeaturesBanner />
+    <Subscribe />
   </>;
 }
